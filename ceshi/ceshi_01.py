2838c90e636aef46451b23b82e1102761812ff2d
chendaliang <piaoyaocdl@163.com>
2020-08-03 09:19:06 +0800

    1

--- ceshi/ceshi_01.py
+++ ceshi/ceshi_01.py
@@ -1,8 +1,11 @@
-from tensorflow.python.keras import Sequential
-from tensorflow.python.keras.layers import Convolution2D, Flatten
+from tensorflow.python.keras.layers import Flatten
 
-model = Sequential()
-model.add(Convolution2D(64, 3, 3, border_mode='same', input_shape=(3, 32, 32)))
-# now: model.output_shape == (None, 64, 32, 32)
-model.add(Flatten())
-# now: model.output_shape == (None, 65536)
+yaping = Flatten()
+yaping([1, 1])
+print(yaping)
+#
+# model = Sequential()
+# model.add(Convolution2D(64, 3, 3, border_mode='same', input_shape=(3, 32, 32)))
+# # now: model.output_shape == (None, 64, 32, 32)
+# model.add(Flatten())
+# # now: model.output_shape == (None, 65536)
