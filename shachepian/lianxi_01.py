2c9dd4bc7b468a2f65fcefebe87195dd2b3cad08
chendaliang <piaoyaocdl@163.com>
2020-08-08 20:11:19 +0800

    1

--- shachepian/lianxi_01.py
+++ shachepian/lianxi_01.py
@@ -2,20 +2,29 @@ from tensorflow import keras
 from tensorflow.keras.preprocessing import image
 from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
 
+#############################################################################################
+# 参数
+#############################################################################################
+
 xunlian_wenjianjia = '/home/c/Desktop/shachepian_shuju/20200612'
 ceshi_wenjianjia = '/home/c/Desktop/shachepian_shuju/ceshi'
 
-xunlianshuju = image.DirectoryIterator(
+#############################################################################################
+# 训练
+#############################################################################################
+
+xunlian_shuju = image.DirectoryIterator(
     directory=xunlian_wenjianjia,
     image_data_generator=ImageDataGenerator(data_format='channels_first', dtype='float64'),
     target_size=(200, 200)
 )
 
-print(xunlianshuju.image_shape)
+print(xunlian_shuju.image_shape)
+print(len(xunlian_shuju))
 
 moxing = keras.Sequential(
     [
-        keras.layers.Flatten(input_shape=xunlianshuju.image_shape),
+        keras.layers.Flatten(input_shape=xunlian_shuju.image_shape),
         keras.layers.Dense(128, activation='relu'),
         keras.layers.Dense(128, activation='relu'),
         keras.layers.Dense(128, activation='relu'),
@@ -29,7 +38,12 @@ moxing.compile(
     metrics=['accuracy']
 )
 
-moxing.fit(x=xunlianshuju, epochs=1)
+for shuju in xunlian_shuju.next():
+    moxing.fit(x=xunlian_shuju, epochs=1)
+
+#############################################################################################
+# 测试
+#############################################################################################
 
 ceshi_shuju = image.DirectoryIterator(
     directory=ceshi_wenjianjia,
