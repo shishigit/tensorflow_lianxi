443aecbadf07fb8a41c61670fc1f953e962309ca
chendaliang <piaoyaocdl@163.com>
2020-08-19 16:05:08 +0800

    1

--- lianxi_03.py
+++ lianxi_03.py
@@ -2,19 +2,21 @@ from tensorflow import keras
 from tensorflow.keras.preprocessing import image
 from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
 
+
 #############################################################################################
 # 参数
 #############################################################################################
+class canshu:
+    wenjianjia_xunlian = '/home/c/Desktop/CODE/shachepian_shuju/20200612'
+    wenjianjia_ceshi = '/home/c/Desktop/CODE/shachepian_shuju/ceshi'
 
-xunlian_wenjianjia = '/home/c/Desktop/shachepian_shuju/20200612'
-ceshi_wenjianjia = '/home/c/Desktop/shachepian_shuju/ceshi'
 
 #############################################################################################
 # 训练数据
 #############################################################################################
 
 xunlian_shuju = image.DirectoryIterator(
-    directory=xunlian_wenjianjia,
+    directory=canshu.wenjianjia_xunlian,
     image_data_generator=ImageDataGenerator(
         data_format='channels_first',
         dtype='float64'
@@ -31,22 +33,22 @@ print('-------------------------------------------------------------------------
 #############################################################################################
 # 训练
 #############################################################################################
-base_model = keras.applications.MobileNetV2(
+qianyimoxing = keras.applications.MobileNetV2(
     input_shape=xunlian_shuju.image_shape,
     include_top=False,
     weights='imagenet',
 )
-base_model.trainable = False
+qianyimoxing.trainable = False
 
 shuru = keras.layers.Input(shape=xunlian_shuju.image_shape)
 x = keras.layers.experimental.preprocessing.Rescaling(1. / 127.5, offset=-1)(shuru)
 x = keras.layers.experimental.preprocessing.RandomFlip('horizontal')(x)
 x = keras.layers.experimental.preprocessing.RandomRotation(0.1)(x)
-x = base_model(x, training=False)
+x = qianyimoxing(x, training=False)
 x = keras.layers.GlobalAveragePooling2D()(x)
-x = keras.layers.Dense(100,activation=keras.activations.relu)(x)
+x = keras.layers.Dense(100, activation=keras.activations.relu)(x)
 x = keras.layers.Dropout(0.2)(x)
-shuchu = keras.layers.Dense(xunlian_shuju.num_classes,activation='softmax')(x)
+shuchu = keras.layers.Dense(xunlian_shuju.num_classes, activation='softmax')(x)
 
 moxing = keras.Model(shuru, shuchu)
 
@@ -68,7 +70,7 @@ moxing.fit(
 #############################################################################################
 
 ceshi_shuju = image.DirectoryIterator(
-    directory=ceshi_wenjianjia,
+    directory=canshu.wenjianjia_ceshi,
     image_data_generator=ImageDataGenerator(data_format='channels_first', dtype='float64'),
 )
 
@@ -76,4 +78,3 @@ moxing.evaluate(
     x=ceshi_shuju,
     verbose=2
 )
-
