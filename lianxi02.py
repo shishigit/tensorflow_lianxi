1e1092a2630880d5d9f561b7adcec9e0fbb62fe4
chendaliang <piaoyaocdl@163.com>
2020-08-23 19:20:24 +0800

    1

--- lianxi02.py
+++ lianxi02.py
@@ -26,6 +26,7 @@ class canshu:
 #############################################################################################
 
 xunlian_shuju = canshu.huoqushujuji(canshu.wenjianjia_xunlian)
+
 qianyimoxing = keras.applications.MobileNetV2(
     input_shape=xunlian_shuju.image_shape,
     include_top=False,
@@ -35,6 +36,9 @@ qianyimoxing.trainable = False
 
 
 def shengchengmoxing(hp):
+    hp_units = hp.Int('units', min_value=32, max_value=512, step=32)
+    hp_learning_rate = hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])
+
     shuru = keras.layers.Input(shape=xunlian_shuju.image_shape)
     x = keras.layers.experimental.preprocessing.Rescaling(1. / 127.5)(shuru)
     x = x - 1
@@ -42,14 +46,12 @@ def shengchengmoxing(hp):
     x = keras.layers.experimental.preprocessing.RandomRotation(0.1)(x)
     x = qianyimoxing(x, training=False)
     x = keras.layers.GlobalAveragePooling2D()(x)
-    hp_units = hp.Int('units', min_value=32, max_value=512, step=32)
-    x = keras.layers.Dense(hp_units, activation=keras.activations.relu)(x)
-    x = keras.layers.Dropout(0.2)(x)
+    for i in range(hp.Int('num_layers', 2, 20)):
+        x = keras.layers.Dense(hp_units, activation=keras.activations.relu)(x)
+        x = keras.layers.Dropout(0.2)(x)
     shuchu = keras.layers.Dense(xunlian_shuju.num_classes, activation='softmax')(x)
 
     moxing = keras.Model(shuru, shuchu)
-
-    hp_learning_rate = hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])
 
     moxing.compile(
         optimizer=keras.optimizers.Adam(learning_rate=hp_learning_rate),
@@ -67,6 +69,7 @@ tuner = kerastuner.Hyperband(
     directory='my_dir',
     project_name='intro_to_kt'
 )
+tuner.search_space_summary()
 
 ceshi_shuju = canshu.huoqushujuji(canshu.wenjianjia_ceshi)
 
@@ -75,6 +78,8 @@ tuner.search(
     epochs=10,
     validation_data=ceshi_shuju
 )
+
+tuner.results_summary()
 
 best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
 
